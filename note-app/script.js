c00fa662bb9c4efd5f4f420250de3cb007607c62
Joffrey LAGUT <j.lagut@live.fr>
2019-07-09 22:03:40 +0200

    Add functions to display and delete notes in the UI
    - Allow the user to add a new note.

--- note-app/script.js
+++ note-app/script.js
@@ -1,15 +1,38 @@
-var notes = [
-  { title: "My first beautiful note", content: "first hello world" },
-  { title: "My second beautiful note", content: "second hello world" }
+// We want to display the note after the page is loaded
+addEventListener("load", () => {
+  displayNoteNodes();
+});
+
+const notes = [
+  { id: 1, title: "My first beautiful note", content: "first hello world" },
+  { id: 2, title: "My second beautiful note", content: "second hello world" }
 ];
 
-var displayNotes = () => {
+const deleteNoteNodes = () => {
   let root = document.getElementById("notes");
-  console.log(root);
-  let nodeToDelete = root.getElementsByTagName("div");
-  while (nodeToDelete.length != 0) {
-    root.removeChild(nodeToDelete[0]);
+  while (root.childElementCount > 0) {
+    root.removeChild(root.firstChild);
   }
 };
 
-displayNotes();
+const displayNoteNodes = () => {
+  deleteNoteNodes();
+  const sortedNotes = notes.sort((a, b) => a.id < b.id);
+  let root = document.getElementById("notes");
+  for (let i = 0; i < sortedNotes.length; i++) {
+    const noteDiv = document.createElement("div");
+    noteDiv.innerHTML = `
+      <p id="note_${notes[i].id}">${notes[i].title}</p>
+    `;
+    root.appendChild(noteDiv);
+  }
+};
+
+const newNote = () => {
+  let last = notes.reduce(
+    (acc, current) => (acc < current.id ? current.id : acc),
+    0
+  );
+  notes.push({ id: ++last, title: "New note" });
+  displayNoteNodes();
+};
