73622d45f48be5f0da61123dc3f87e6420c33ec0
Joffrey Lagut <j.lagut@live.fr>
2019-07-11 22:29:04 +0200

    Add the ability to display a note content. WIP Impossible to save ATM

---
+++ note-app/readme.md
@@ -0,0 +1,5 @@
+# Setup
+
+``
+gulp browser sync
+`` 
--- note-app/script.js
+++ note-app/script.js
@@ -22,7 +22,7 @@ const displayNoteNodes = () => {
   for (let i = 0; i < sortedNotes.length; i++) {
     const noteDiv = document.createElement("div");
     noteDiv.innerHTML = `
-      <p id="note_${notes[i].id}">${notes[i].title}</p>
+      <p class="note-listitem" id="note_${notes[i].id}" onclick="showNoteDetails(${notes[i].id})">${notes[i].title}</p>
     `;
     root.appendChild(noteDiv);
   }
@@ -36,3 +36,16 @@ const newNote = () => {
   notes.push({ id: ++last, title: "New note" });
   displayNoteNodes();
 };
+
+const showNoteDetails = (id) => {
+  let note = notes.find(n => n.id === id);
+  document.getElementById('note-title').innerHTML = `<input type="text" value="${note.title}"></input>`;
+  document.getElementById('note-content').innerHTML = `<p>${note.content}</p>`;
+  document.getElementById('note-controls').innerHTML = `<button onclick="saveNote(${note.id})">Save</button>`;
+}
+
+const saveNote = (id) => {
+  let i = notes.findIndex(n => n.id === id);
+  notes[i] = { id: id, title: document.getElementById('note-title').value, content: document.getElementById('note-content').value };
+  displayNoteNodes();
+}
