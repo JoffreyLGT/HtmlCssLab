499cf62566342d86f77ee3b51d490286fc11c3fa
Joffrey LAGUT <j.lagut@live.fr>
2019-07-22 17:06:11 +0200

    Auto save during note edition
    - Add JSDoc to the function.

--- note-app/script.js
+++ note-app/script.js
@@ -37,7 +37,6 @@ const newNote = () => {
   notes.push({ id, title: "New note", content: "" });
   displayNoteNodes(id);
   showNoteDetails(id);
-  console.log(notes);
 };
 
 const showNoteDetails = (id) => {
@@ -46,18 +45,39 @@ const showNoteDetails = (id) => {
     previousSelected.className = 'note-listitem';
   }
 
-  document.getElementById(`note_${id}`).className = 'note-listitem-selected';
   let note = notes.find(n => n.id === id);
   document.getElementById('note-title').value = note.title;
   document.querySelector('#note-content textarea').value = note.content;
-  let saveDiv = document.getElementById('save-note');
-  saveDiv.innerHTML = `<button>Save</button>`;
-  saveDiv.querySelector('button').addEventListener('click', () => saveNote(note.id));
+
+  let $selectedNote = document.getElementById(`note_${id}`)
+  $selectedNote.className = 'note-listitem-selected';
+  document.querySelectorAll('#note-title, #note-content textarea').forEach(element => {
+    // Save 1 sec after the last keyup.
+    element.addEventListener('keyup', () => saveAfterTimeout(note.id, $selectedNote, 1000));
+    // Immediately save when the change event is triggered.
+    // Mandatory to be sure to save when the user select another note.
+    element.addEventListener('change', () => saveAfterTimeout(note.id, $selectedNote, 0));
+  });
 }
 
-const saveNote = (id) => {
-  let i = notes.findIndex(n => n.id === id);
-  notes[i].title = document.getElementById('note-title').value;
-  notes[i].content = document.getElementById('#note-content textarea').value;
-  displayNoteNodes(id);
+/**
+ * Save the note after the timeout is triggered.
+ * 
+ * @param {id} string id of the element in the dom
+ * @param {$note} nodeelement reference to the node in the list of notes
+ */
+const saveAfterTimeout = (id, $note, timeInMs) => {
+  if (globalTimeout != null) {
+    clearTimeout(globalTimeout);
+  }
+  let title = document.getElementById('note-title').value;
+  globalTimeout = setTimeout(function () {
+    globalTimeout = null;
+    $note.innerHTML = title;
+    let i = notes.findIndex(n => n.id === id);
+    notes[i].title = title;
+    notes[i].content = document.querySelector('#note-content textarea').value;
+
+  }, timeInMs);
 }
+var globalTimeout = null; // Mandatory for the function above.
