db321fbb4104626734c64f3423485fec344ed39d
Joffrey LAGUT <joff@macbook-pro-de-joffrey.home>
2019-08-17 17:57:08 +0200

    Add the live timer

--- wedding/script.js
+++ wedding/script.js
@@ -1,23 +1,38 @@
-// const weddingDate = new Date('2020-08-22T16:00:00').getTime();
+const startCountdown = () => {
+  const $days = document.getElementById("timer-days");
+  const $hours = document.getElementById("timer-hours");
+  const $minutes = document.getElementById("timer-minutes");
+  const $seconds = document.getElementById("timer-seconds");
 
-// const startCountdown = () => {
-//     setTimeout(countdown, 0);
-// }
+  const weddingDate = new Date("2020-08-22T16:00:00").getTime();
+  const timeRemaining = weddingDate - new Date().getTime();
+  let days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-// const countdown = () => {
-//     console.log("Countdown started");
-//     const $days = document.getElementById('timer-days');
-//     const $hours = document.getElementById('timer-hours');
-//     const $minutes = document.getElementById('timer-minutes');
-//     const $seconds = document.getElementById('timer-seconds');
+  setInterval(() => {
+    if (seconds > 0) {
+      seconds--;
+    } else if (minutes > 0) {
+      seconds = 59;
+      minutes--;
+    } else if (hours > 0) {
+      seconds = 59;
+      minutes = 59;
+      hours--;
+    } else if (days > 0) {
+      seconds = 59;
+      minutes = 59;
+      hours = 23;
+      days--;
+    }
 
-//     let remaining = 1;
-//     while(remaining != 0){
-//         setTimeout(()=>{
-//             // TODO Set the correct values in the fields
-//             remaining = weddingDate - new Date().getTime();
-//             $days.innerText = Math.floor(remaining / (1000 * 60 * 60 * 24));
-//             console.log("tic toc")
-//         }, 1000);
-//     }
-// }
+    $days.innerText = days.toString().padStart(3, "0");
+    $hours.innerText = hours.toString().padStart(2, "0");
+    $minutes.innerText = minutes.toString().padStart(2, "0");
+    $seconds.innerText = seconds.toString().padStart(2, "0");
+  }, 1000);
+};
